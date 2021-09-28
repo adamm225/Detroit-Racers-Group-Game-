f43cf23b92e74f7f1fce89c28c7d816c79b6d2a7
XavusZookie <91273624+XavusZookie@users.noreply.github.com>
2021-09-27 21:39:30 -0400

    scores
    added everything we need for score and did a rough outline of the code for whoever is going to do the car and restart animation

--- Detroit Racers/Assets/Scripts/scoremanager.cs
+++ Detroit Racers/Assets/Scripts/scoremanager.cs
@@ -12,13 +12,18 @@ public class scoremanager : MonoBehaviour
 
     public Text scoretext;
     public Text hiscoretext;
+    public Image flames;
 
-    public bool isincreasing;
+    public bool isincreasing = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(PlayerPrefs.GetFloat("highscore") != null)
+            {
+            hiscorecount = PlayerPrefs.GetFloat("highscore");
+            }
     }
 
     // Update is called once per frameasdfas
@@ -29,12 +34,21 @@ public class scoremanager : MonoBehaviour
             scorecount += pointspersecond * Time.deltaTime;
         }
 
-        if(scorecount > hiscorecount)
+        if(scorecount >= hiscorecount)
         {
             hiscorecount = scorecount;
+            PlayerPrefs.SetFloat("highscore", hiscorecount);
+            flames.enabled = true;
+
         }
 
         scoretext.text = "Score: " + Mathf.Round(scorecount);
         hiscoretext.text = "High Score: " + Mathf.Round(hiscorecount);
+    }
+
+    public void resetthehighscore()
+    {
+        PlayerPrefs.SetFloat("highscore", 0f);
+        hiscorecount = 0;
     }
 }
