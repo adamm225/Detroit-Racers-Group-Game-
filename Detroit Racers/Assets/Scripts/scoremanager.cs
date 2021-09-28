877ac3660f9bba5a5d158736fa8ad1620337c402
XavusZookie <91273624+XavusZookie@users.noreply.github.com>
2021-09-27 20:04:12 -0400

    started scores
    got the main scores started without data saving

---
+++ Detroit Racers/Assets/Scripts/scoremanager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoremanager : MonoBehaviour
+{
+    public float scorecount;
+    public float hiscorecount;
+
+    public float pointspersecond;
+
+    public Text scoretext;
+    public Text hiscoretext;
+
+    public bool isincreasing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isincreasing)
+        { 
+            scorecount += pointspersecond * Time.deltaTime;
+        }
+
+        if(scorecount > hiscorecount)
+        {
+            hiscorecount = scorecount;
+        }
+
+        scoretext.text = "Score: " + Mathf.Round(scorecount);
+        hiscoretext.text = "High Score: " + Mathf.Round(hiscorecount);
+    }
+}
