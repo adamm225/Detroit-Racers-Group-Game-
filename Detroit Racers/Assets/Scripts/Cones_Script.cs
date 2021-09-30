faeda132aec1af402e7f2b8806d552cb7a336ca4
Diskpart1 <90070203+Diskpart1@users.noreply.github.com>
2021-09-29 21:51:27 -0400

    Banana Random - Generator

--- Detroit Racers/Assets/Scripts/Cones_Script.cs
+++ Detroit Racers/Assets/Scripts/Cones_Script.cs
@@ -8,8 +8,10 @@ public class Cones_Script : MonoBehaviour
     Vector3 scale;
     
     // Start is called before the first frame update
+    int num;
     void Start()
     {
+        num = 2;
         pos = transform.position;
        // transform.position = new Vector3(0, 0, 0);
         scale = transform.localScale;
@@ -17,27 +19,44 @@ public class Cones_Script : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update()  
+    {  
+
+        Debug.Log(num); // print number
         if(Time.timeScale == 1f){
-            Debug.Log(transform.position.y);
+            //Debug.Log(transform.position.y); //
             if (transform.position.y >= -5 )
             {
                 transform.localScale = transform.localScale + new Vector3(.011f, .011f, 0);
                 //transform.position = transform.position + new Vector3(-.01f, -.011f, 0);
-                transform.position = transform.position + new Vector3(0, -.011f, 0);
+                //transform.position = transform.position + new Vector3(0, -.011f, 0);             
+                if (num == 1)
+                {
+                    transform.position = transform.position + new Vector3(-.021f, -.011f, 0);
+                }
+                if (num == 2)
+                {
+                    transform.position = transform.position + new Vector3(0, -.011f, 0);
+                }
+                if (num == 3)
+                {
+                    transform.position = transform.position + new Vector3(.021f, -.011f, 0);
+                }
             }
             else
-            {
+            {   //This is once it gets to car (if y IS NOT >= -5)
                 transform.position = transform.position + new Vector3(0, -.30f, 0);
             }
-            if (transform.position.y <= -10)
+            
+            if (transform.position.y <= -10)  //Then it goes back to where it was
             {   
                 transform.position = pos;
                 transform.localScale = scale;
                 
+                //Generate random number 1-3
+                num = Random.Range(1,4); // Numbers between 1-3
             } 
-        }
+        }   
     }
 
 }
--- Detroit Racers/Assets/Scripts/TreemoveRight.cs
+++ Detroit Racers/Assets/Scripts/TreemoveRight.cs
@@ -19,7 +19,7 @@ public class TreemoveRight : MonoBehaviour
     void Update()
     {
 
-        Debug.Log(transform.position);
+        //Debug.Log(transform.position);
         transform.position += new Vector3(0.01f, -0.004f, 0);
         transform.localScale += new Vector3(0.1f, 0.1f, 0);
         if (transform.position.y < -7)
